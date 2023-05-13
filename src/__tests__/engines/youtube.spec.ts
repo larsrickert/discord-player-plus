cb9c89f130b1b57207b24cfb5277703952ecea53
Lars Rickert <dev@lars-rickert.de>
2023-05-13 13:01:50 +0200

    chore: fix unit tests (#13)

--- src/__tests__/engines/youtube.spec.ts
+++ src/__tests__/engines/youtube.spec.ts
@@ -1,137 +1,37 @@
-import {
-  playlist_info,
-  search,
-  stream,
-  YouTubeChannel,
-  YouTubePlayList,
-  YouTubeVideo,
-} from "play-dl";
-import { describe, expect, it, vi } from "vitest";
-import { mock } from "vitest-mock-extended";
+import playDl, { YouTubePlayList, YouTubeStream, YouTubeVideo } from "play-dl";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import { mock, mockClear } from "vitest-mock-extended";
+import { Playlist, Track } from "../..";
 import { youtubeEngine } from "../../engines/youtube";
-import { Playlist, Track } from "../../types/engines";
 
-const expectedTrack = {
-  url: "testUrl",
-  title: "testTitle",
-  duration: 123,
-  source: "youtube",
-  artist: "testArtist",
-  thumbnailUrl: "testThumbnail",
-} satisfies Track;
+describe.concurrent("youtube engine", () => {
+  const mockVideo = mock<YouTubeVideo>({
+    title: "testTitle",
+    durationInSec: 123,
+    channel: { name: "testArtist" },
+    url: "testUrl",
+    thumbnails: [{ url: "testThumbnail" }],
+  });
 
-const expectedPlaylist = {
-  title: "testPlaylistTitle",
-  url: "testPlaylistUrl",
-  thumbnailUrl: "testPlaylistThumbnail",
-} satisfies Playlist;
-
-vi.mock("play-dl", async (importOriginal) => {
-  const module: Record<string, unknown> = await importOriginal();
-
-  return {
-    ...module,
-    stream: vi.fn(),
-    search: vi.fn(() => [
-      {
-        ...mock<YouTubeVideo>(),
-        url: expectedTrack.url,
-        title: expectedTrack.title,
-        durationInSec: expectedTrack.duration,
-        channel: {
-          ...mock<YouTubeChannel>(),
-          name: expectedTrack.artist,
-        },
-        thumbnails: [
-          {
-            height: 0,
-            width: 0,
-            url: expectedTrack.thumbnailUrl,
-            toJSON: vi.fn(),
-          },
-        ] as YouTubeVideo["thumbnails"],
-      },
-    ]),
-    playlist_info: vi.fn(() => ({
-      ...mock<YouTubePlayList>(),
-      title: expectedPlaylist.title,
-      url: expectedPlaylist.url,
-      thumbnail: {
-        height: 0,
-        width: 0,
-        url: expectedPlaylist.thumbnailUrl,
-        toJSON: vi.fn(),
-      } as YouTubePlayList["thumbnail"],
-      page: vi.fn(() =>
-        new Array(100).fill("").map(() => ({
-          ...mock<YouTubeVideo>(),
-          url: expectedTrack.url,
-          title: expectedTrack.title,
-          durationInSec: expectedTrack.duration,
-          channel: {
-            ...mock<YouTubeChannel>(),
-            name: expectedTrack.artist,
-          },
-          thumbnails: [
-            {
-              height: 0,
-              width: 0,
-              url: expectedTrack.thumbnailUrl,
-              toJSON: vi.fn(),
-            },
-          ] as YouTubeVideo["thumbnails"],
-        }))
-      ),
-      next: vi.fn((limit?: number) =>
-        new Array(limit ?? 100).fill("").map(() => ({
-          ...mock<YouTubeVideo>(),
-          url: expectedTrack.url,
-          title: expectedTrack.title,
-          durationInSec: expectedTrack.duration,
-          channel: {
-            ...mock<YouTubeChannel>(),
-            name: expectedTrack.artist,
-          },
-          thumbnails: [
-            {
-              height: 0,
-              width: 0,
-              url: expectedTrack.thumbnailUrl,
-              toJSON: vi.fn(),
-            },
-          ] as YouTubeVideo["thumbnails"],
-        }))
-      ),
-      all_videos: vi.fn(() =>
-        new Array(250).fill("").map(() => ({
-          ...mock<YouTubeVideo>(),
-          url: expectedTrack.url,
-          title: expectedTrack.title,
-          durationInSec: expectedTrack.duration,
-          channel: {
-            ...mock<YouTubeChannel>(),
-            name: expectedTrack.artist,
-          },
-          thumbnails: [
-            {
-              height: 0,
-              width: 0,
-              url: expectedTrack.thumbnailUrl,
-              toJSON: vi.fn(),
-            },
-          ] as YouTubeVideo["thumbnails"],
-        }))
-      ),
-    })),
+  const expectedTrack: Track = {
+    artist: "testArtist",
+    duration: 123,
+    source: "youtube",
+    thumbnailUrl: "testThumbnail",
+    title: "testTitle",
+    url: "testUrl",
   };
-});
 
-describe.concurrent("YouTube engine", () => {
-  it("has source", () => {
+  beforeEach(() => {
+    mockClear(mockVideo);
+    vi.resetAllMocks();
+  });
+
+  test("should have correct source", () => {
     expect(youtubeEngine.source).toBe("youtube");
   });
 
-  it.each([
+  test.each([
     "https://www.youtube.com/",
     "https://youtube.com/",
     "https://youtu.be/",
@@ -146,104 +46,120 @@ describe.concurrent("YouTube engine", () => {
     expect(isResponsible).toBe(true);
   });
 
-  it("should search track", async () => {
-    const result = await youtubeEngine.search(expectedTrack.url, {}, {});
-    expect(search).toHaveBeenCalledWith(expectedTrack.url, {
-      source: { youtube: "video" },
-    });
-    expect(result.length).toBeGreaterThanOrEqual(1);
-    expect(result[0].source).toBe(youtubeEngine.source);
-    expect(result[0].tracks.length).toBe(1);
-    expect(result[0].tracks[0]).toEqual(expectedTrack);
+  test("should not return search results for empty query", async () => {
+    const results = await youtubeEngine.search("   ", {}, {});
+    expect(results).toStrictEqual([]);
   });
 
-  it("should handle search with minimal video information", async () => {
-    const minimalVideo = {
-      url: expectedTrack.url,
-      chapters: [],
-      durationInSec: expectedTrack.duration,
-      durationRaw: "",
-      likes: 0,
-      live: false,
-      private: false,
-      tags: [],
-      thumbnails: [],
-      toJSON: vi.fn(),
-      toString: vi.fn(),
-      type: "video",
-      views: 0,
-    } satisfies YouTubeVideo;
+  test("should return search results for video query", async () => {
+    const searchSpy = vi.spyOn(playDl, "search").mockResolvedValue([mockVideo]);
 
-    const expected = {
-      url: expectedTrack.url,
-      duration: expectedTrack.duration,
-      source: expectedTrack.source,
-      title: "",
-    } satisfies Track;
+    const query = "https://www.youtube.com/watch?v=testVideoId";
 
-    const playDl = await import("play-dl");
-    vi.spyOn(playDl, "search").mockReturnValue(Promise.resolve([minimalVideo]));
-
-    const result = await youtubeEngine.search(minimalVideo.url, {}, {});
-    expect(search).toHaveBeenCalledWith(expectedTrack.url, {
-      source: { youtube: "video" },
-    });
-    expect(result.length).toBeGreaterThanOrEqual(1);
-    expect(result[0].source).toBe(youtubeEngine.source);
-    expect(result[0].tracks.length).toBe(1);
-    expect(result[0].tracks[0]).toEqual(expected);
-  });
-
-  it.each([
-    "https://www.youtube.com/watch?v=H5v3kku4y6Q&list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-    "https://www.youtube.com/playlist?list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-    "https://music.youtube.com/watch?v=H5v3kku4y6Q&list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-  ])("should search playlist %s", async (query) => {
     const results = await youtubeEngine.search(query, {}, {});
-    expect(playlist_info).toHaveBeenCalledWith(query, { incomplete: true });
-    expect(results.length).toBeGreaterThanOrEqual(1);
 
-    const result = results[0];
-    expect(result).toBeDefined();
-
-    expect(result.source).toBe(youtubeEngine.source);
-    expect(result.tracks.length).toBe(250);
-    expect(result.tracks[0]).toEqual({
-      ...expectedTrack,
-      playlist: expectedPlaylist,
+    expect(searchSpy).toHaveBeenCalledWith(query, {
+      source: { youtube: "video" },
     });
-    expect(result.playlist).toEqual(expectedPlaylist);
-    expect(result.playlist).toBe(result.tracks[0].playlist);
+    expect(results).toStrictEqual([
+      {
+        source: "youtube",
+        tracks: [expectedTrack],
+      },
+    ]);
   });
 
-  it("searches tracks with limit", async () => {
+  test("should return search results for playlist query", async () => {
+    const playlistInfoSpy = vi.spyOn(playDl, "playlist_info").mockResolvedValue(
+      mock<YouTubePlayList>({
+        all_videos: vi.fn(() => Promise.resolve([mockVideo])),
+        title: "testPlaylistTitle",
+        url: "testPlaylistUrl",
+        thumbnail: { url: "testPlaylistThumbnail" },
+      })
+    );
+
+    const validateSpy = vi
+      .spyOn(playDl, "yt_validate")
+      .mockReturnValue("playlist");
+
+    const query = "https://www.youtube.com/playlist?list=testPlaylistId";
+
+    const results = await youtubeEngine.search(query, {}, {});
+
+    const expectedPlaylist: Playlist = {
+      title: "testPlaylistTitle",
+      url: "testPlaylistUrl",
+      thumbnailUrl: "testPlaylistThumbnail",
+    };
+
+    expect(validateSpy).toHaveBeenCalledWith(query);
+    expect(playlistInfoSpy).toHaveBeenCalledWith(query, { incomplete: true });
+
+    expect(results).toStrictEqual([
+      {
+        source: "youtube",
+        playlist: expectedPlaylist,
+        tracks: [{ ...expectedTrack, playlist: expectedPlaylist }],
+      },
+    ]);
+  });
+
+  test("searches tracks with limit", async () => {
+    const searchSpy = vi.spyOn(playDl, "search").mockResolvedValue([mockVideo]);
+
     const limit = 4;
     await youtubeEngine.search(expectedTrack.url, {}, { limit });
 
-    expect(search).toHaveBeenCalledWith(expectedTrack.url, {
+    expect(searchSpy).toHaveBeenCalledWith(expectedTrack.url, {
       source: { youtube: "video" },
       limit,
     });
   });
 
-  it("searches playlist with limit", async () => {
-    const limit = 128;
-    const searchResults = await youtubeEngine.search(
-      "https://www.youtube.com/playlist?list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-      {},
-      { limit }
-    );
+  test("searches playlist with limit", async () => {
+    const mockPlaylist = mock<YouTubePlayList>({
+      all_videos: vi.fn(() => Promise.resolve([mockVideo])),
+      page: vi.fn(() => [mockVideo]),
+      next: vi.fn(() => Promise.resolve([mockVideo])),
+      title: "testPlaylistTitle",
+      url: "testPlaylistUrl",
+      thumbnail: { url: "testPlaylistThumbnail" },
+    });
 
-    const result = searchResults[0];
-    expect(result).toBeDefined();
-    expect(result.tracks.length).toBe(limit);
+    vi.spyOn(playDl, "playlist_info").mockResolvedValue(mockPlaylist);
+    vi.spyOn(playDl, "yt_validate").mockReturnValue("playlist");
+
+    const query = "https://www.youtube.com/playlist?list=somePlaylistId";
+
+    // limit < 100
+    await youtubeEngine.search(query, {}, { limit: 42 });
+    expect(mockPlaylist.page).toHaveBeenCalledWith(1);
+    expect(mockPlaylist.all_videos).not.toHaveBeenCalled();
+
+    // limit > 100
+    mockClear(mockPlaylist);
+    await youtubeEngine.search(query, {}, { limit: 128 });
+    expect(mockPlaylist.next).toHaveBeenCalledWith(28);
+    expect(mockPlaylist.all_videos).not.toHaveBeenCalled();
+
+    // no limit
+    mockClear(mockPlaylist);
+    await youtubeEngine.search(query, {}, { limit: 0 });
+    expect(mockPlaylist.all_videos).toHaveBeenCalled();
   });
 
-  it("gets stream", async () => {
-    await youtubeEngine.getStream(expectedTrack, { quality: "high" });
-    expect(stream).toHaveBeenCalledWith(expectedTrack.url, {
+  test("gets stream", async () => {
+    const mockStream = mock<YouTubeStream>();
+    const streamSpy = vi.spyOn(playDl, "stream").mockResolvedValue(mockStream);
+
+    const stream = await youtubeEngine.getStream(expectedTrack, {
+      quality: "high",
+    });
+    expect(streamSpy).toHaveBeenCalledWith(expectedTrack.url, {
       quality: 2,
       seek: undefined,
     });
+    expect(stream).toStrictEqual(mockStream);
   });
 });
